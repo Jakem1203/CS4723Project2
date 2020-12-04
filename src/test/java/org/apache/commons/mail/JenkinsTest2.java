cb6597c58b267bb4903dc89202dc367e8f2fd8cd
Jacob Moschkau <47117444+Jakem1203@users.noreply.github.com>
2020-12-04 16:36:54 -0600

    Update JenkinsTest2.java

--- src/test/java/org/apache/commons/mail/JenkinsTest2.java
+++ src/test/java/org/apache/commons/mail/JenkinsTest2.java
@@ -1,5 +1,10 @@
 package org.apache.commons.mail;
 
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import junit.framework.TestCase;
 import org.junit.Test;
 
 public class JenkinsTest2 extends TestCase {
